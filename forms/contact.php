f3d525f1e278cb0a6d285645830120e55fcc5522
aarondavidgepersonal <114311538+aarondavidgepersonal@users.noreply.github.com>
2022-10-03 23:31:25 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,20 +1,3 @@
 <?php
 
-
-$name = $POST['name'];
-$email = $POST['email'];
-$subject = $POST['subject'];
-$message = $POST['message'];
-
-
-$mailheader = "From:".$name."<".$email.">\r\n";
-
-$recipient = "aarondavidgejob@gmail.com";
-
-if(mail($recipient,$subject,$message,$mailheader)){
-    echo 'Your mail has been sent successfully.';
-} else{
-    echo 'Unable to send email. Please try again.';
-}
-
-?>
+phpinfo();
