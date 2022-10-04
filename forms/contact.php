92b783f449c28efcf4d7d992c8537f2bc4dfa847
aarondavidgepersonal <114311538+aarondavidgepersonal@users.noreply.github.com>
2022-10-04 00:08:54 -0500

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,5 +1,13 @@
 <?php
 
-phpinfo();
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+echo '<h1>'. $name .'</h1>';
+echo '<h2>'. $email .'</h2>';
+echo '<h3>'. $subject .'</h3>';
+echo '<h4>'. $message .'</h4>';
 
 ?>
