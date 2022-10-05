8722d569ad54ee1f9e13448ffe12ea0ab5f79987
aarondavidgepersonal <114311538+aarondavidgepersonal@users.noreply.github.com>
2022-10-04 22:30:30 -0500

    server side validation test
    server side validation test

--- forms/contact.php
+++ forms/contact.php
@@ -5,9 +5,40 @@ $email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-echo '<h1>'. $name .'</h1>';
-echo '<h2>'. $email .'</h2>';
-echo '<h3>'. $subject .'</h3>';
-echo '<h4>'. $message .'</h4>';
+function cleanform($name, $email, $subject, $message)
+{
 
-?>
+    $name = preg_replace("/[^a-zA-Z0-9]+/", "", $name);
+    $subject = preg_replace("/[^a-zA-Z0-9]+/", "", $subject);
+    $message = preg_replace("/[^a-zA-Z0-9]+/", "", $message);
+
+    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) && $name != '' && $subject != '' && $message != '') {
+
+        writetodatabase($name, $email, $message, $subject);
+        sendmymail($name, $email, $message, $subject);
+    }
+}
+
+cleanform($name, $email, $message, $subject);
+
+function sendmymail($name, $email, $message, $subject)
+{
+
+    echo 'Your send mail function was activated.';
+
+    echo '<h1>Your name, ' . $name . ' was sent to email.</h1>';
+    echo '<h1>Your email, ' . $email . ' was sent to email.</h1>';
+    echo '<h1>Your subject, ' . $subject . ' was sent to email.</h1>';
+    echo '<h1>Your message, ' . $message . ' was sent to email.</h1>';
+}
+
+function writetodatabase($name, $email, $message, $subject)
+{
+
+    echo 'Your write to database function was activated.';
+
+    echo '<h1>Your name, ' . $name . ' was written to database.</h1>';
+    echo '<h1>Your email, ' . $email . ' was written to database.</h1>';
+    echo '<h1>Your subject, ' . $subject . ' was written to database.</h1>';
+    echo '<h1>Your message, ' . $message . ' was written to database.</h1>';
+}
